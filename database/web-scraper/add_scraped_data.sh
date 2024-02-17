89f1915d125f4b688495cce03ca2fa0876e60ef1
taylor-cox <taylor@coxintl.com>
2024-02-17 17:16:13 -0500

    made setup even better

--- database/web-scraper/add_scraped_data.sh
+++ database/web-scraper/add_scraped_data.sh
@@ -3,6 +3,6 @@ if ![ -d "./venv" ]; then
   echo "Creating virtual environment"
   python3 -m venv venv
 fi
-. ./venv/bin/activate
+source ./venv/bin/activate
 pip install -r requirements.txt
 python add_scraped_content_to_db.py
--- initial_setup.sh
+++ initial_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "Running this application requires Spotify Client ID and Secret. Please enter them below:"
-read -p "Spotify Client ID: " SPOTIFY_CLIENT_ID
-read -p "Spotify Client Secret: " SPOTIFY_CLIENT_SECRET
-touch ./api/.spotify-env
-printf "SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID\nSPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET" > ./api/.spotify-env
+if [ ! -f "./api/.spotify-env" ]; then
+  echo "Running this application requires Spotify Client ID and Secret. Please enter them below:"
+  read -p "Spotify Client ID: " SPOTIFY_CLIENT_ID
+  read -p "Spotify Client Secret: " SPOTIFY_CLIENT_SECRET
+  touch ./api/.spotify-env
+  printf "SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID\nSPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET" > ./api/.spotify-env
+fi
 
 echo "Generating SSL certs for API and Website"
 cd ./api/ssl-certs
@@ -18,6 +20,7 @@ sleep 15
 cd ./web-scraper
 pwd
 ./add_scraped_data.sh
+echo "Added scraped data to database."
 cd ..
 pwd
 docker compose down
